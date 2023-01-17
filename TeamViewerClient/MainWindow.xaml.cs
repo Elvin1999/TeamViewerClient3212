efcfd494ea49469b8e2c6f015a08fa24775cb235
camalzadeelvin <camalzadeelvin@STHQ0122-01>
2023-01-17 21:41:47 +0400

    Add project files.

---
+++ TeamViewerClient/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeamViewerClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                App.Current.Dispatcher.BeginInvoke(() =>
+                {
+
+
+                    NetworkHelper.Start(IpTxtbox.Text, int.Parse(PortTxtbox.Text));
+    
+                }); ;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TeamViewerClient/NetworkHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TeamViewerClient
+{
+    internal class NetworkHelper
+    {
+        public static void Start(string ipString,int port)
+        {
+            var client = new TcpClient();
+            var ip = IPAddress.Parse(ipString);
+            var ep = new IPEndPoint(ip, port);
+
+            try
+            {
+                client.Connect(ep);
+                if (client.Connected)
+                {
+                    MessageBox.Show("Connected");
+                    var writer = Task.Run(() =>
+                    {
+                        while (true)
+                        {
+                            var text = "I Connect";
+                            var stream = client.GetStream();
+                            var bw = new BinaryWriter(stream);
+                            bw.Write(text);
+                        }
+                    });
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
+
